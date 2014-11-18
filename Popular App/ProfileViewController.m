c149d1636bc2cf8b9769159b0ca12379e9e4dd9a
Andrew Liu <dieseliu321@gmail.com>
2014-11-17 20:31:42 -0800

    iboutlet

--- Popular App/ProfileViewController.m
+++ Popular App/ProfileViewController.m
@@ -7,7 +7,54 @@
 //
 
 #import "ProfileViewController.h"
+#import "PhotoCollectionViewCell.h"
+
+@interface ProfileViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
+@property (weak, nonatomic) IBOutlet UILabel *photoCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *followingCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *followerCountLabel;
+@property NSMutableArray *arrayOfPhoto;
+
+@end
 
 @implementation ProfileViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.arrayOfPhoto = [@[]mutableCopy];
+
+
+
+
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+//TODO: delete photo from parse
+
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+
+    return self.arrayOfPhoto.count;
+
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    NSData *data = self.arrayOfPhoto[indexPath.item];
+
+    UIImage *image = [UIImage imageWithData:data];
+    cell.imageView.image = image;
+    return cell;
+}
 
 @end
