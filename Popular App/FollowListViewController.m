0825c4580747f1546295f78f95fe569cf32050ca
Andrew Liu <dieseliu321@gmail.com>
2014-11-17 21:53:03 -0800

    followlistviewcontroller

---
+++ Popular App/FollowListViewController.m
@@ -0,0 +1,21 @@
+//
+//  FollowListViewController.m
+//  Popular App
+//
+//  Created by Andrew Liu on 11/17/14.
+//  Copyright (c) 2014 May Yang. All rights reserved.
+//
+
+#import "FollowListViewController.h"
+
+@interface FollowListViewController ()
+
+@end
+
+@implementation FollowListViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+@end
