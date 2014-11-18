44425e8e9dbfaf5ee8e25c9a58d21e7a689820fb
May Yang <mayy.yang@gmail.com>
2014-11-17 18:32:35 -0800

    created storyboard

--- Popular App/RootViewController.m
+++ Popular App/RootViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2014 May Yang. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RootViewController.h"
 
-@interface ViewController ()
+@interface RootViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
