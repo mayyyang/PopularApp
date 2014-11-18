83017268f92c23b7448fb95b3c09fc0f33669c02
Andrew Liu <dieseliu321@gmail.com>
2014-11-17 19:33:03 -0800

    uicollectionview delegate  setup

---
+++ Popular App/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  Popular App
+//
+//  Created by Andrew Liu on 11/17/14.
+//  Copyright (c) 2014 May Yang. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
--- Popular App/RootViewController.m
+++ Popular App/RootViewController.m
@@ -7,21 +7,55 @@
 //
 
 #import "RootViewController.h"
+#import "PhotoCollectionViewCell.h"
 
-@interface RootViewController ()
+@interface RootViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentControl;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property NSMutableArray *arrayOfRecentPhoto;
+@property NSMutableArray *arrayOfPopularPhoto;
 
 @end
 
 @implementation RootViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.arrayOfRecentPhoto = [@[]mutableCopy];
+    self.arrayOfPopularPhoto = [@[]mutableCopy];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    if (self.segmentControl == 0)
+    {
+        return self.arrayOfRecentPhoto.count;
+    }
+    else
+    {
+        return self.arrayOfPopularPhoto.count;
+    }
 }
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    NSData *data = [NSData new];
+    if (self.segmentControl == 0)
+    {
+        data = self.arrayOfRecentPhoto[indexPath.item];
+    }
+    else
+    {
+        data = self.arrayOfPopularPhoto[indexPath.item];
+    }
+    UIImage *image = [UIImage imageWithData:data];
+    cell.imageView.image = image;
+    return cell;
+}
+
 
 @end
