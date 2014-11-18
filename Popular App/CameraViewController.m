6c2fda9d59dbb32653f82615df8abbf6750868f6
Andrew Liu <dieseliu321@gmail.com>
2014-11-18 08:56:55 -0800

    camera init

--- Popular App/CameraViewController.m
+++ Popular App/CameraViewController.m
@@ -9,10 +9,21 @@
 #import "CameraViewController.h"
 
 
-@interface CameraViewController ()
+@interface CameraViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
 @implementation CameraViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
 
 @end
